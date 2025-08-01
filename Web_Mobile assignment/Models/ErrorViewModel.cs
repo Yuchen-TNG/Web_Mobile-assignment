4a3beb0cbae488f28b914ebfdf495aaf340d9eef
Yuchen-TNG <tngyc-wm24@student.tarc.edu.my>
2025-08-01 12:52:53 +0800

    Add project files.

---
+++ Web_Mobile assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_Mobile_assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
